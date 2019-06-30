b7d2c3b8b77165134d02204d457625509d8aac99
dkl187788 <13783459623@163.com>
2019-06-30 17:04:59 +0800

    git commed demo first commit

---
+++ src/main/java/com/dukl/git/PersonService.java
@@ -0,0 +1,9 @@
+package com.dukl.git;
+
+/**
+ * Created by adu on 2019/6/30.
+ */
+public interface PersonService {
+    void eat();
+    void sleep();
+}
---
+++ src/main/java/com/dukl/git/PersonServiceImpl.java
@@ -0,0 +1,16 @@
+package com.dukl.git;
+
+/**
+ * Created by adu on 2019/6/30.
+ */
+public class PersonServiceImpl implements PersonService {
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+}
